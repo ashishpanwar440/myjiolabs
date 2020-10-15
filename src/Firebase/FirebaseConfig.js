2374eb2626902bdd45822bf0903932852b3b8290
ashishpanwar440 <32572930+ashishpanwar440@users.noreply.github.com>
2020-10-16 01:21:21 +0530

    Update FirebaseConfig.js

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCq3wJPt2Fp0XYedf2_Pf6GQmXIhVUgYZ8",
-    authDomain: "myjiolabs.firebaseapp.com",
-    databaseURL: "https://myjiolabs.firebaseio.com",
-    projectId: "myjiolabs",
-    storageBucket: "myjiolabs.appspot.com",
-    messagingSenderId: "148802284032",
-    appId: "1:148802284032:web:4bba23cec67a3b31d546e5",
-    measurementId: "G-FF1RC8HY88"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DB_URL",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESS_ID",
+    appId: "APP_ID",
+    measurementId: "MEASURE_ID"
 }
 
 firebase.initializeApp (firebaseConfig)
